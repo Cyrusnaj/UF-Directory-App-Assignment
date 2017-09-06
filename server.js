24e1cc3f99ed29fed6d8caa434a266f7332a04e3
Cyrus Najmi <cyrusnaj@ufl.edu>
2017-09-06 00:39:27 -0400

    Completed Assignment

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-var http = require('http'), 
-    fs = require('fs'), 
+var http = require('http'),
+    fs = require('fs'),
     url = require('url'),
     port = 8080;
 
@@ -10,17 +10,30 @@ var requestHandler = function(request, response) {
   var parsedUrl = url.parse(request.url);
 
   /*
-    Your request handler should send listingData in the JSON format if a GET request 
-    is sent to the '/listings' path. Otherwise, it should send a 404 error. 
-
-    HINT: explore the request object and its properties 
+    Your request handler should send listingData in the JSON format if a GET request
+    is sent to the '/listings' path. Otherwise, it should send a 404 error.
+    HINT: explore the request object and its properties
     http://stackoverflow.com/questions/17251553/nodejs-request-object-documentation
    */
+
+   if (request['method'] == 'GET' && parsedUrl.pathname == '/listings'){
+    response.writeHead(200, {"Content-Type": "application/json"});
+    console.log(listingData)
+    response.end(listingData);
+    } else {
+    response.writeHead(404, {"Content-Type": "application/json"});
+    response.end("Bad gateway error");
+  }
 };
+
+  var server = http.createServer(requestHandler);
+  server.listen(port, function(){
+    console.log("server listening on: http://localhost:" + port);
+});
 
 fs.readFile('listings.json', 'utf8', function(err, data) {
   /*
-    This callback function should save the data in the listingData variable, 
-    then start the server. 
+    This callback function should save the data in the listingData variable,
+    then start the server.
    */
 });
